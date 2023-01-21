60648ade7b731eb388ca94ff05aab88cdd475d8f
s26407-pj <92219393+s26407-pj@users.noreply.github.com>
2023-01-21 11:26:44 +0000

    Add files via upload

---
+++ src/main/java/pl/pjatk/s26407Bank/S26407BankApplication.java
@@ -0,0 +1,13 @@
+package pl.pjatk.s26407Bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S26407BankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S26407BankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/pjatk/s26407Bank/S26407BankApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pjatk.s26407Bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S26407BankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
